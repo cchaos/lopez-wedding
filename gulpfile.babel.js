a840e3863a2ce13b8a643d802f6f4f0bfc72e0fb
Caroline Horn <caroline@curiouschaos.com>
2016-02-17 20:33:46 -0500

    Using MDL assets

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -109,8 +109,28 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
+      // Component handler
+      './bower_components/material-design-lite/src/mdlComponentHandler.js',
+      // Base components
+      './bower_components/material-design-lite/src/button/button.js',
+      './bower_components/material-design-lite/src/checkbox/checkbox.js',
+      './bower_components/material-design-lite/src/icon-toggle/icon-toggle.js',
+      './bower_components/material-design-lite/src/menu/menu.js',
+      './bower_components/material-design-lite/src/progress/progress.js',
+      './bower_components/material-design-lite/src/radio/radio.js',
+      './bower_components/material-design-lite/src/slider/slider.js',
+      './bower_components/material-design-lite/src/spinner/spinner.js',
+      './bower_components/material-design-lite/src/switch/switch.js',
+      './bower_components/material-design-lite/src/tabs/tabs.js',
+      './bower_components/material-design-lite/src/textfield/textfield.js',
+      './bower_components/material-design-lite/src/tooltip/tooltip.js',
+      // Complex components (which reuse base components)
+      './bower_components/material-design-lite/src/layout/layout.js',
+      './bower_components/material-design-lite/src/data-table/data-table.js',
+      // And finally, the ripples
+      './bower_components/material-design-lite/src/ripple/ripple.js',
+      // Other scripts,
       './app/scripts/main.js'
-      // Other scripts
     ])
       .pipe($.newer('.tmp/scripts'))
       .pipe($.sourcemaps.init())
